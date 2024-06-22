551113b9dacb0b9720ed3c65da2750f0b0aab63e
Nahian27 <alnpulok27@gmail.com>
2024-06-22 21:35:06 +0600

    Cloudflare pages env vars working

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,23 +1,39 @@
-import type {MetaFunction} from "@remix-run/cloudflare";
-import {Card} from "~/components/ui/card";
+import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/cloudflare";
+import axios from "axios";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
-    return [
-        {title: "TELESCOPE 🔭 - Dashboard"},
-        {
-            name: "description",
-            content: "Welcome to my app!",
-        },
-    ];
+  return [
+    { title: "New Remix App" },
+    {
+      name: "description",
+      content: "Welcome to Remix on Cloudflare!",
+    },
+  ];
 };
 
+export async function loader({ context }: LoaderFunctionArgs) {
+  const Axios = axios.create({ baseURL: context.cloudflare.env.API_URL });
+
+  const res = await Axios.get("/issue");
+  return res.data;
+}
+
 export default function Index() {
-    return (
-        <>
-            Dashboard
-            <Card>
-                hello
-            </Card>
-        </>
-    );
+  const { data } = useLoaderData<typeof loader>();
+  return (
+    <div className="font-sans p-4">
+      <h1 className="text-3xl">Welcome to Remix on Cloudflare</h1>
+      {data.map(
+        (
+          item: {
+            title: string;
+          },
+          i: number
+        ) => (
+          <p key={i}>{item.title}</p>
+        )
+      )}
+    </div>
+  );
 }
--- load-context.ts
+++ load-context.ts
@@ -1,5 +1,15 @@
 import { type PlatformProxy } from "wrangler";
 
+// When using `wrangler.toml` to configure bindings,
+// `wrangler types` will generate types for those bindings
+// into the global `Env` interface.
+// Need this empty interface so that typechecking passes
+// even if no `wrangler.toml` exists.
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface Env {
+  API_URL: string | undefined;
+}
+
 type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
 
 declare module "@remix-run/cloudflare" {
